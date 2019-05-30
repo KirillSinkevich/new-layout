565e31831ad3994a37253416acd90c4b27153e2f
kirillSinkevich <kirill@uniquite.com>
2019-05-30 17:56:19 +0300

    fixed button

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ $(document).ready(() => {
 	var posBlock = document.querySelector('.pos-block');
 	var iScreenMain = document.querySelector('.l-screen--main');
 	var iContPosition = document.querySelector('.l-cont--position');
+	var iBtn = document.querySelector('.i-btn');
 	var iScreenMainHeight = iScreenMain.clientHeight;
 	
 	//posBlock.style.marginTop = (iScreenMainHeight * 0.245) + 'px';
@@ -30,27 +31,30 @@ $(document).ready(() => {
 		posBlock.style.height = (iScreenMainHeight * 0.13) + 'px';
 	}*/
 	
-	
 	$(window).resize(() => {
 		var windowHeight = document.body.clientHeight;
 		var iScreenMainHeight = iScreenMain.clientHeight;
 		var iScreenMainWidth = iScreenMain.clientWidth;
-		var ratio = iScreenMainWidth / iScreenMainHeight;
-		
+		var wRatio = iScreenMainWidth / iScreenMainHeight;
+		var hRatio = iScreenMainHeight / iScreenMainWidth;
+		console.log('wRatio', wRatio);
 		//posBlock.style.marginTop = (iScreenMainHeight * 0.245) + 'px';
 		posBlock.style.width = (iScreenMainHeight * 0.321) + 'px';
 		posBlock.style.height = (iScreenMainHeight * 0.1) + 'px';
-		//ratio >= 1.75 && ratio <= 1.76
+		
+		if (wRatio >= 1.71 && wRatio <= 1.79) {
 			iContPosition.style.width = iScreenMainWidth + 'px';
 			iContPosition.style.height = iScreenMainHeight + 'px';
-		if (ratio >= 1.71 && ratio <= 1.79) {
-			console.log('ok');
-		} else {
-			/*iContPosition.style.width = 1423 + 'px';
-			iContPosition.style.height = 812 + 'px';*/
+		} else if (wRatio < 1.71) {
+			//iContPosition.style.width = iScreenMainWidth + 'px';
+			iContPosition.style.height = iScreenMainHeight + 'px';
+		} else if (wRatio > 1.79) {
+			iContPosition.style.width = iScreenMainWidth + 'px';
+			iBtn.style.marginTop = 0;
+			posBlock.style.marginTop = 15 + '%';
+			//iContPosition.style.height = iScreenMainHeight + 'px';
 		}
-		
-		
+
 		/*if (windowHeight <= 810) {
 			//posBlock.style.marginLeft = (iScreenMainHeight * 0.36) / -2 + 'px';
 			posBlock.style.marginTop = (iScreenMainHeight * 0.29) + 'px';
