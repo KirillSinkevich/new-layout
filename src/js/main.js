132d61cfca2e69dc70f90834eecbcf8aba40bae8
kirillSinkevich <kirill@uniquite.com>
2019-06-10 19:43:56 +0300

    add photo and comleted studio

--- src/js/main.js
+++ src/js/main.js
@@ -13,21 +13,45 @@ $(document).ready(() => {
 	var iContPosition = document.querySelector('.l-cont--position');
 	var posBlock = document.querySelector('.pos-block');
 	var iBtn = document.querySelector('.i-btn');
+	var choiceItemsSize = document.querySelectorAll('.l-choice__items-size');
 	var iScreenMainHeight = iScreenMain.clientHeight;
 	var iScreenMainWidth = iScreenMain.clientWidth;
+	var windowHeight = document.body.clientHeight;
 	var btnWidth = 260;
 	var btnHeight = 90;
-	var imgHeight = 812;
-	var imgWidth = 1427;
+	var imgMainHeight = 812;
+	var imgMainWidth = 1427;
+	var imgChoiceHeidth = 323;
+	var imgChoiceWidth = 359;
 	
+	console.log('windowHeight', windowHeight);
+	
+	console.log('choiceItemsSize', choiceItemsSize);
+	
+	function resizeImg(imgSide, screenSide) {
+		var ratioImgWidth = imgChoiceWidth / imgSide;
+		var ratioImgHeight = imgChoiceHeidth / imgSide;
+		console.log('ratioImgWidth', ratioImgWidth);
+		console.log('ratioImgHeight', ratioImgHeight);
+		console.log('string', (screenSide * ratioImgWidth) + 'px');
+		
+		choiceItemsSize.forEach((elt) => {
+			//console.log('elt', elt);
+			elt.style.width = (screenSide * ratioImgWidth) + 'px';
+			elt.style.height = (screenSide * ratioImgHeight) + 'px';
+		})
+		// choiceItemsSize.style.width = (screenSide * ratioImgWidth) + 'px';
+		// choiceItemsSize.style.height = (screenSide * ratioImgHeight) + 'px';
+	}
+	resizeImg(imgMainHeight, windowHeight);
 	function resizeBtn(imgSide, screenSide) {
 		var ratioBtnWidth = btnWidth / imgSide;
 		var ratioBtnHeight = btnHeight / imgSide;
 		posBlock.style.width = (screenSide * ratioBtnWidth) + 'px';
 		posBlock.style.height = (screenSide * ratioBtnHeight) + 'px';
 	}
-	resizeBtn(imgHeight, iScreenMainHeight);
-	posBlock.style.marginTop = (iScreenMainHeight * 0.245) + 'px';
+	//resizeBtn(imgMainHeight, iScreenMainHeight);
+	//posBlock.style.marginTop = (iScreenMainHeight * 0.245) + 'px';
 	
 	$(".l-awards__wrap-content").mCustomScrollbar({
 		scrollInertia: 500,
@@ -36,11 +60,15 @@ $(document).ready(() => {
 	$(window).resize(() => {
 		var iScreenMainHeight = iScreenMain.clientHeight;
 		var iScreenMainWidth = iScreenMain.clientWidth;
+		var windowHeight = document.body.clientHeight;
 		var wRatio = iScreenMainWidth / iScreenMainHeight;
 		//console.log('wRatio', wRatio);
-		posBlock.style.marginTop = (iScreenMainHeight * 0.245) + 'px';
+		//posBlock.style.marginTop = (iScreenMainHeight * 0.245) + 'px';
 		
-		if (wRatio >= 1.71 && wRatio <= 1.79) {
+		resizeImg(imgMainHeight, windowHeight);
+		
+		
+		/*if (wRatio >= 1.71 && wRatio <= 1.79) {
 			iContPosition.style.width = iScreenMainWidth + 'px';
 			iContPosition.style.height = iScreenMainHeight + 'px';
 			resizeBtn(imgHeight, iScreenMainHeight);
@@ -51,7 +79,7 @@ $(document).ready(() => {
 			iBtn.style.marginTop = 0;
 			iContPosition.style.width = iScreenMainWidth + 'px';
 			posBlock.style.marginTop = 14.1 + '%';
-			resizeBtn(imgWidth, iScreenMainWidth);
-		}
+			resizeBtn(imgMainWidth, iScreenMainWidth);
+		}*/
 	});
 });
